0f462fbef2f7d80dfad21442e86b3bc9a37ebb25
yangzhen <616366255@qq.com>
2020-03-25 15:57:01 +0800

    create lmdb for SDR4k

--- codes/data_scripts/create_lmdb.py
+++ codes/data_scripts/create_lmdb.py
@@ -15,15 +15,18 @@ import utils.util as util  # noqa: E402
 
 
 def main():
-    dataset = 'DIV2K_demo'  # vimeo90K | REDS | general (e.g., DIV2K, 291) | DIV2K_demo |test
-    mode = 'GT'  # used for vimeo90k and REDS datasets
+    dataset = 'SDR4k'  # vimeo90K | REDS | general (e.g., DIV2K, 291) | DIV2K_demo |test
+    mode = '4bit'  # used for vimeo90k and REDS datasets
     # vimeo90k: GT | LR | flow
     # REDS: train_sharp, train_sharp_bicubic, train_blur_bicubic, train_blur, train_blur_comp
     #       train_sharp_flowx4
+    # SDR4k: 10bit, 4bit
     if dataset == 'vimeo90k':
         vimeo90k(mode)
     elif dataset == 'REDS':
         REDS(mode)
+    elif dataset == 'SDR4k':
+        SDR4k(mode)
     elif dataset == 'general':
         opt = {}
         opt['img_folder'] = '../../datasets/DIV2K/DIV2K800_sub'
@@ -405,6 +408,96 @@ def test_lmdb(dataroot, dataset='REDS'):
     C, H, W = [int(s) for s in meta_info['resolution'].split('_')]
     img = img_flat.reshape(H, W, C)
     cv2.imwrite('test.png', img)
+
+
+def SDR4k(mode):
+    """Create lmdb for the REDS dataset, each image with a fixed size
+    10bit: [3, 2160, 3840], key: 00000000_000
+    4bit: [3, 2160, 3840], key: 00000000_000
+    """
+    #### configurations
+    read_all_imgs = False  # whether real all images to memory with multiprocessing
+    # Set False for use limited memory
+    BATCH = 5000  # After BATCH images, lmdb commits, if read_all_imgs = False
+    if mode == "10bit":
+        img_folder = "..\\..\\datasets\\SDR_10bit"  # for windows
+        # lmdb_save_path = "..\\..\\datasets\\SDR_10bit.lmdb"  # for windows
+        lmdb_save_path = "../../datasets/SDR_10bit.lmdb"  # for linux
+        H_dst, W_dst = 2160, 3840
+    elif mode == "4bit":
+        img_folder = "..\\..\\datasets\\SDR_4bit"  # for windows
+        lmdb_save_path = "..\\..\\datasets\\SDR_4bit.lmdb"
+        H_dst, W_dst = 2160, 3840
+    n_thread = 40
+    ########################################################
+    if not lmdb_save_path.endswith('.lmdb'):
+        raise ValueError("lmdb_save_path must end with \'lmdb\'.")
+    if osp.exists(lmdb_save_path):
+        print('Folder [{:s}] already exists. Exit...'.format(lmdb_save_path))
+        sys.exit(1)
+
+    #### read all the image paths to a list
+    print('Reading image path list ...')
+    all_img_list = data_util._get_paths_from_images(img_folder)
+    keys = []
+    for img_path in all_img_list:
+        # split_rlt = img_path.split('/')
+        split_rlt = img_path.split("\\")  # for windows
+        folder = split_rlt[-2]
+        img_name = split_rlt[-1].split('.png')[0]
+        keys.append(folder + '_' + img_name)
+
+    if read_all_imgs:
+        #### read all images to memory (multiprocessing)
+        dataset = {}  # store all image data. list cannot keep the order, use dict
+        print('Read images with multiprocessing, #thread: {} ...'.format(n_thread))
+        pbar = util.ProgressBar(len(all_img_list))
+
+        def mycallback(arg):
+            '''get the image data and update pbar'''
+            key = arg[0]
+            dataset[key] = arg[1]
+            pbar.update('Reading {}'.format(key))
+
+        pool = Pool(n_thread)
+        for path, key in zip(all_img_list, keys):
+            pool.apply_async(read_image_worker, args=(path, key), callback=mycallback)
+        pool.close()
+        pool.join()
+        print('Finish reading {} images.\nWrite lmdb...'.format(len(all_img_list)))
+
+    #### create lmdb environment
+    data_size_per_img = cv2.imread(all_img_list[0], cv2.IMREAD_UNCHANGED).nbytes
+    print('data size per image is: ', data_size_per_img)
+    data_size = data_size_per_img * len(all_img_list)
+    env = lmdb.open(lmdb_save_path, map_size=data_size * 10)
+
+    #### write data to lmdb
+    print("Start writing...")
+    pbar = util.ProgressBar(len(all_img_list))
+    txn = env.begin(write=True)
+    for idx, (path, key) in enumerate(zip(all_img_list, keys)):
+        pbar.update('Write {}'.format(key))
+        key_byte = key.encode('ascii')
+        data = dataset[key] if read_all_imgs else cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        H, W, C = data.shape
+        assert H == H_dst and W == W_dst and C == 3, 'different shape.'
+        txn.put(key_byte, data)
+        if not read_all_imgs and idx % BATCH == 0:
+            txn.commit()
+            txn = env.begin(write=True)
+    txn.commit()
+    env.close()
+    print('Finish writing lmdb.')
+
+    #### create meta information
+    meta_info = {}
+    meta_info['name'] = 'SDR4k_{}'.format(mode)
+    channel = 3
+    meta_info['resolution'] = '{}_{}_{}'.format(channel, H_dst, W_dst)
+    meta_info['keys'] = keys
+    pickle.dump(meta_info, open(osp.join(lmdb_save_path, 'meta_info.pkl'), "wb"))
+    print('Finish creating lmdb meta info.')
 
 
 if __name__ == "__main__":
